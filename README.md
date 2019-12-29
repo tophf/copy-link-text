6cefccf143e1972dbcedd0cecb25acf11f434172
tophf <tophf@gmx.com>
2019-12-29 14:18:08 +0300

    seed

---
+++ README.md
@@ -0,0 +1,3 @@
+Copies the link text via context menu.
+
+It works fully on demand, acquires access only to the active tab when selected in the menu, and doesn't even require a permanent content script on all URLs unlike other similar extensions.
---
+++ bg.js
@@ -0,0 +1,31 @@
+﻿'use strict';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: 'run',
+    type: 'normal',
+    contexts: ['link'],
+    title: 'Copy link te&xt',
+    documentUrlPatterns: ['*://*/*', 'file://*/*'],
+  }, () => chrome.runtime.lastError);
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  chrome.tabs.executeScript(tab.id, {
+    frameId: info.frameId,
+    runAt: 'document_start',
+    code: 'document.activeElement.innerText',
+  }, ([text]) => {
+    if (!chrome.runtime.lastError && text)
+      copyToClipboard(text);
+  });
+});
+
+function copyToClipboard(text) {
+  const el = document.createElement('textarea');
+  document.body.appendChild(el);
+  el.value = text;
+  el.select();
+  document.execCommand('copy');
+  el.remove();
+}
