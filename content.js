8145cb86f73d12144e8cea57f25478aae466887f
tophf <tophf@gmx.com>
2022-04-19 12:00:04 +0300

    http pages don't have navigator.clipboard

--- content.js
+++ content.js
@@ -4,5 +4,22 @@
   let el;
   let root = document;
   while ((el = root.activeElement) && (root = el.shadowRoot)) {}
-  navigator.clipboard.writeText(el.innerText);
+  const text = el.innerText;
+  const {clipboard} = navigator;
+  if (clipboard) {
+    clipboard.writeText(text);
+  } else {
+    const sel = getSelection();
+    const {anchorNode, anchorOffset, focusNode, focusOffset} = sel;
+    const el = document.createElement('textarea');
+    el.style.cssText = 'all:revert; position:fixed; top: 0; left: 0; visibility:hidden;'
+      .replace(/;/g, '!important');
+    el.value = text;
+    document.body.appendChild(el);
+    el.focus({preventScroll: true});
+    el.select();
+    document.execCommand('copy');
+    el.remove();
+    sel.setBaseAndExtent(anchorNode, anchorOffset, focusNode, focusOffset);
+  }
 }
