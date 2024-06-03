e38cc41eaf28817259f24858e05f0562e678ed5d
Nick Bolton <nick@symless.com>
2024-06-03 11:57:34 +0100

    Add support for links that aren't active elements

--- bg.js
+++ bg.js
@@ -1,19 +1,30 @@
-﻿'use strict';
+﻿"use strict";
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
-    id: 'run',
-    type: 'normal',
-    contexts: ['link'],
-    title: 'Copy link te&xt',
-    documentUrlPatterns: ['*://*/*', 'file://*/*'],
+    id: "run",
+    type: "normal",
+    contexts: ["link"],
+    title: "Copy link te&xt",
+    documentUrlPatterns: ["*://*/*", "file://*/*"],
   });
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  chrome.scripting.executeScript({
-    target: {tabId: tab.id, frameIds: [info.frameId]},
-    files: ['content.js'],
-    // injectImmediately: true, // TODO: Chrome 102
-  }).catch(() => {});
+  console.log("Context menu clicked", { info, tab });
+
+  chrome.scripting
+    .executeScript({
+      target: { tabId: tab.id, frameIds: [info.frameId] },
+      files: ["content.js"],
+      // injectImmediately: true, // TODO: Chrome 102
+    })
+    .then(() => {
+      const message = { linkUrl: info.linkUrl };
+      console.log("Content script executed, sending message:", message);
+      chrome.tabs.sendMessage(tab.id, message);
+    })
+    .catch((error) => {
+      console.error("Failed to execute content script", error);
+    });
 });
--- content.js
+++ content.js
@@ -1,30 +1,67 @@
-'use strict';
+"use strict";
 
-{
-  let el;
+chrome.runtime.onMessage.addListener((request) => {
+  console.log("Link URL:", request.linkUrl);
+
+  try {
+    copyLinkText(request.linkUrl);
+  } catch (error) {
+    console.error("Failed to copy link text:", error);
+  }
+});
+
+function copyLinkText(linkUrl) {
+  console.log("Copying link text");
+  const active = findActiveElement();
+  console.log("Active element: ", active);
+  const link = findLinkElement(active, linkUrl);
+  console.log("Link element: ", link);
+  const text = link.innerText;
+  writeClipboard(text);
+}
+
+function findLinkElement(activeElement, linkUrl) {
+  if (activeElement.tagName === "A") {
+    return activeElement;
+  }
+
+  // Happens in Jira, because of their super complex pages :)
+  console.warn("Active element is not a link, finding element based on URL:", linkUrl);
+  const links = Array.from(document.querySelectorAll("a"));
+  const results = links.filter((link) => link.href === linkUrl);
+
+  if (results.length === 0) throw new Error("Link element not found");
+
+  if (results.length > 1) {
+    console.warn("Multiple link elements found, returning first:", results);
+  }
+
+  if (!results[0]) throw new Error("Link results found, but first element is falsy");
+
+  return results[0];
+}
+
+function findActiveElement() {
+  let active;
   let root = document;
   const getRoot = chrome.dom?.openOrClosedShadowRoot;
-  while (
-    (el = root.activeElement) &&
-    (root = el.shadowRoot || (getRoot ? getRoot(el) : el.openOrClosedShadowRoot))
-  ) {}
-  const text = el.innerText;
-  const {clipboard} = navigator;
-  if (clipboard) {
-    clipboard.writeText(text);
-  } else {
-    const sel = getSelection();
-    const {anchorNode, anchorOffset, focusNode, focusOffset} = sel;
-    const t = document.createElement('textarea');
-    t.style.cssText = 'all:revert; position:fixed; top: 0; left: 0; visibility:hidden;'
-      .replace(/;/g, '!important');
-    t.value = text;
-    document.body.appendChild(t);
-    t.focus({preventScroll: true});
-    t.select();
-    document.execCommand('copy');
-    el.focus();
-    t.remove();
-    sel.setBaseAndExtent(anchorNode, anchorOffset, focusNode, focusOffset);
+
+  // Refactored version of the original fork's code... not sure if this is the best way.
+  active = root.activeElement;
+  while (active) {
+    root = active.shadowRoot || (getRoot ? getRoot(active) : active.openOrClosedShadowRoot);
+    if (!root) break;
+
+    active = root.activeElement;
   }
+
+  return active;
+}
+
+// The original fork had a hacky backup way of copying to the clipboard which was removed.
+function writeClipboard(text) {
+  console.log("Writing to clipboard: ", text);
+  const { clipboard } = navigator;
+  if (!clipboard) throw new Error("Clipboard API not available");
+  clipboard.writeText(text);
 }
